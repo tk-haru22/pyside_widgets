df30a5e78c1c84e641fc0c33bc31daf933accb61
sapporo22 <66178176+sapporo22@users.noreply.github.com>
2024-12-01 18:58:39 +0900

    change mvc_combobx.py

--- mvc_combobox.py
+++ mvc_combobox.py
@@ -16,23 +16,22 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
-
-items = [
-    'root/parent-item1/child-item1',
-    'root/parnet-item1/child-item2',
-    'root/parnet-item1/child-item3',
-    'root/parnet-item1/child-item4',
-    'root/parnet-item1/child-item5',
-    'root/parnet-item2/child-item6',
-    'root/parnet-item2/child-item7',
-    'root/parnet-item2/child-item8',
-    'root/parnet-item2/child-item9',
-    'root/parnet-item2/child-item10',
-    'root/parnet-item3/child-item11',
-    'root/parnet-item3/child-item12',
-    'root/parnet-item3/child-item13',
-]
-
+"""
+Items
+    root/parent-item1/child-item1
+    root/parnet-item1/child-item3
+    root/parnet-item1/child-item4
+    root/parnet-item1/child-item2
+    root/parnet-item1/child-item5
+    root/parnet-item2/child-item6
+    root/parnet-item2/child-item7
+    root/parnet-item2/child-item8
+    root/parnet-item2/child-item9
+    root/parnet-item2/child-item10
+    root/parnet-item3/child-item11
+    root/parnet-item3/child-item12
+    root/parnet-item3/child-item13
+"""
 
 class Window(QMainWindow):
 
