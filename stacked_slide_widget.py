897a8e943cdccc330f1aa7f678db3964fd9ef886
tk-haru22 <htakada.120220@gmail.com>
2025-02-26 21:39:31 +0900

    update stacked_slide_widget.py

---
+++ stacked_slide_widget.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import (
+    QWidget,
+    QMainWindow,
+    QStackedWidget,
+    QPushButton,
+    QLabel,
+)
+
+class StackedSlideWidget(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
